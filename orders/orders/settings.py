b410c51e715ecf12e6235fa45934dfeaac3744a7
Obihiro-Mk <westmakoto@mail.ru>
2023-03-18 22:37:15 +0300

    2

--- orders/orders/settings.py
+++ orders/orders/settings.py
@@ -134,7 +134,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# AUTH_USER_MODEL = 'backend.User'
+AUTH_USER_MODEL = 'backend.User'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # EMAIL_USE_TLS = True
